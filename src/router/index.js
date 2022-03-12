65ffa3417e454f19232a16f05ca7302ca911ade0
钟政方 <1035684305@qq.com>
2022-03-12 14:59:47 +0800

    增加漏斗图、大致做出文字滚波效果、增加了边框

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Game from '@/components/question-game'
-import scrolling from '@/components/scrolling'
+import scrolling from '@/components/scroll'
 
 const routes = [{
   path: '/game',
