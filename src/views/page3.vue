32867d2dffb28e2903e1ae45d97351a89be2ddab
钟政方 <1035684305@qq.com>
2022-03-28 21:27:16 +0800

    合并了场馆

--- src/views/page3.vue
+++ src/views/page3.vue
@@ -221,9 +221,25 @@ export default {
 
 <style lang="less" scoped>
 .content {
-  height: 100vh;
-  width: 100vw;
-  background: url(@/assets/bj4.jpg);
+background: url(@/assets/bj4.jpg);
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		min-width: 1000px;
+		z-index: -10;
+		zoom: 1;
+		background-color: #fff;
+		background-repeat: no-repeat;
+		background-size: cover;
+		-webkit-background-size: cover;
+		-o-background-size: cover;
+		background-position: center 0;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		min-height: 70vh;
 
   .topb {
     left: 50%;
